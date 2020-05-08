6930836665fafbad0efc5cc80c2f08d33a894d60
Vitaly Liber <zenamax@gmail.com>
2020-05-08 22:41:10 +0300

    fixed

--- components/Card.js
+++ components/Card.js
@@ -1,8 +1,10 @@
+import moment from 'moment';
+
 import Photo from "./Photo";
 
 function Card({ item }) {
   console.log(item);
-  const { image, telegram_username, stack, username, experience } = item;
+  const { image, telegram_username, stack, username, experience, updated_at } = item;
   return (
     <>
       <div className="card m-2 mb-3 m-sm-0 mb-sm-3">
@@ -13,6 +15,7 @@ function Card({ item }) {
             <span className="badge badge-primary mr-2">{el}</span>
           ))}
           <p>{experience}</p>
+          <small><p className="text-muted">{moment(updated_at).fromNow()}</p></small>
           <a
             href={`tg://resolve?domain=${telegram_username}`}
             className="btn btn-success btn-sm btn-block"
