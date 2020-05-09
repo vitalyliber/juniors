2ceed82e5c8a37c2894e03aacae15549d43fb057
Vitaly Liber <zenamax@gmail.com>
2020-05-09 19:14:57 +0300

    fixed

--- components/Card.js
+++ components/Card.js
@@ -4,24 +4,38 @@ import Photo from "./Photo";
 
 function Card({ item }) {
   console.log(item);
-  const { image, telegram_username, stack, username, experience, updated_at } = item;
+  const { image, telegram_username, stack, username, experience, updated_at, github_url } = item;
   return (
     <>
       <div className="card m-2 mb-3 m-sm-0 mb-sm-3">
         <Photo item={image} />
         <div className="card-body">
           <h3>{username}</h3>
-          {stack.replace(/,/g,'').split(" ").map((el) => (
-            <span className="badge badge-primary mr-2">{el}</span>
-          ))}
+          {stack
+            .replace(/,/g, "")
+            .split(" ")
+            .map((el) => (
+              <span className="badge badge-primary mr-2">{el}</span>
+            ))}
           <p className="mt-2">{experience}</p>
-          <small><p className="text-muted">{moment(updated_at).fromNow()}</p></small>
+          <small>
+            <p className="text-muted">{moment(updated_at).fromNow()}</p>
+          </small>
           <a
             href={`tg://resolve?domain=${telegram_username}`}
             className="btn btn-success btn-sm btn-block"
           >
             Telegram
           </a>
+          {github_url && github_url.length > 0 && (
+            <a
+              href={github_url}
+              target="_blank"
+              className="btn btn-outline-dark btn-sm btn-block"
+            >
+              GitHub
+            </a>
+          )}
         </div>
       </div>
     </>
